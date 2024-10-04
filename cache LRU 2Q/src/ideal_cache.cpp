0b13e7d166a0eddfbc3fbfa9a01a22d06c088b2c
mafioznic <zapobedu1075@gmail.com>
2024-10-05 00:35:25 +0300

    fix ideal cache strategy

--- cache LRU 2Q/src/ideal_cache.cpp
+++ cache LRU 2Q/src/ideal_cache.cpp
@@ -4,16 +4,13 @@
 #include <ideal_cache.hpp>
 #include <LRU-2Q.hpp>
 
-
-static TPage find_latest_page (TPage* pages, int currentPageNumber, int numberOfPages, Hashtable & map);
-
 static TPage* get_data (int* numberOfPages);
 
 static void new_page (TPage* pages, int currentPageNumber, int numberOfPages, Hashtable & map, cacheList & lst, int & hits);
 
-static TPage find_latest_page (TPage* pages, int currentPageNumber, int numberOfPages, Hashtable & map);
+static TPage find_latest_page (TPage* pages, int currentPageNumber, int numberOfPages, Hashtable & map, cacheList & list);
 
-bool seek_in_future (TPage* pages, int currentPageNumber, int numberOfPages, size_t listSize);
+// bool seek_in_future (TPage* pages, int currentPageNumber, int numberOfPages, size_t listSize);
 
 using namespace std;
 
@@ -55,67 +52,96 @@ int count_hits_Ideal_cache () {
 void new_page (TPage* pages, int currentPageNumber, int numberOfPages, Hashtable & map, cacheList & lst, int & hits) {
 
     // printf("\nelem - %d\n", pages[currentPageNumber]);
-    if (lst.lst.size() == lst.listSize && 
-        seek_in_future(pages, currentPageNumber, numberOfPages, lst.listSize) == false &&
-        map.count(pages[currentPageNumber]) == 0) {
-        return;
-    }
+    // if (lst.lst.size() == lst.listSize && 
+    //     seek_in_future(pages, currentPageNumber, numberOfPages, lst.listSize) == false &&
+    //     map.count(pages[currentPageNumber]) == 0) {
+    //     return;
+    // }
 
     if (map.count(pages[currentPageNumber]) == 0) {
 
         if (lst.lst.size() < lst.listSize) {
+
+            // printf("\nPUSH - %d\n", pages[currentPageNumber]);
 
             lst.lst.push_front(pages[currentPageNumber]);
             map.insert({pages[currentPageNumber], lst.lst.begin()});
         }
         else {
 
-            TPage latestPage = find_latest_page(pages, currentPageNumber, numberOfPages, map);
+            TPage latestPage = find_latest_page(pages, currentPageNumber, numberOfPages, map, lst);
 
-            if (latestPage == -1) {
+            if (latestPage == pages[currentPageNumber]) {
                 
-                lst.lst.pop_back();
-                map.erase(lst.lst.back());
+                // printf("\nIGNORE - %d\n", pages[currentPageNumber]);
                 return;
             }
 
+            // printf("\nDELETE - %d and\n", latestPage);
             lst.lst.erase(map.find(latestPage)->second);
             map.erase(latestPage);
 
+            // printf("PUSH - %d\n", pages[currentPageNumber]);
             lst.lst.push_front(pages[currentPageNumber]);
             map.insert({pages[currentPageNumber], lst.lst.begin()});        
         }
     }
     else {
 
+        // printf ("\nHIT\n");
         hits += 1;
     }
 }
 
-TPage find_latest_page (TPage* pages, int currentPageNumber, int numberOfPages, Hashtable & map) {
+TPage find_latest_page (TPage* pages, int currentPageNumber, int numberOfPages, Hashtable & map, cacheList & list) {
 
-    TPage latestPage = -1;
+    List lstCp = list.lst;
+    lstCp.push_back(pages[currentPageNumber]);
+
+    for (auto p : lstCp) {
+
+        bool check = false;
+        for (int i = currentPageNumber + 1; i < numberOfPages; i++) {
+
+            if (pages[i] == p) {
+
+                check = true;
+            }
+        }    
+
+        if (check == false) {
+
+            return p;
+        }
+    }
+
+    TPage latestPage = pages[currentPageNumber];
     for (int i = currentPageNumber + 1; i < numberOfPages; i++) {
 
         if (map.count(pages[i]) != 0) {
 
             latestPage = pages[i];
         }
+
+        else if (pages[i] == pages[currentPageNumber]) {
+
+            latestPage = pages[currentPageNumber];
+        }
     }
     return latestPage;
 }
 
-bool seek_in_future (TPage* pages, int currentPageNumber, int numberOfPages, size_t listSize) {
+// bool seek_in_future (TPage* pages, int currentPageNumber, int numberOfPages, size_t listSize) {
 
-    // cout << pages[currentPageNumber];
-    for (int i = currentPageNumber + 1; /*(i < currentPageNumber + 1 + listSize) && */(i < numberOfPages); i++) {
+//     // cout << pages[currentPageNumber];
+//     for (int i = currentPageNumber + 1; /*(i < currentPageNumber + 1 + listSize) && */(i < numberOfPages); i++) {
 
-        if (pages[i] == pages[currentPageNumber]) {
+//         if (pages[i] == pages[currentPageNumber]) {
 
-            // cout << " true" << endl;
-            return true;
-        }
-    }
-    // cout << " false" << endl;
-    return false;
-}
+//             // cout << " true" << endl;
+//             return true;
+//         }
+//     }
+//     // cout << " false" << endl;
+//     return false;
+// }
