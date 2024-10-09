be66933502e1abd19255d71e51a50581b5ee863c
RedkinDenis <zapobedu1075@gmail.com>
2024-10-09 21:02:04 +0200

    slow version ideal cache

--- cache LRU 2Q/include/ideal_cache.hpp
+++ cache LRU 2Q/include/ideal_cache.hpp
@@ -1,6 +1,83 @@
 #ifndef IDEAL_CACHE_HPP
 #define IDEAL_CACHE_HPP
 
-int count_hits_Ideal_cache ();
+#include "LRU-2Q.hpp"
+
+#include <vector>
+#include<algorithm>
+#include<iterator>
+
+class idealCache {
+
+    public:
+
+    std::vector<TPage> cache;
+    size_t sz;
+
+    idealCache (size_t setSz) : sz(setSz) {}
+
+    int check_hits_ideal_cache(std::vector<TPage> &pages) {
+
+        int pagesCount = pages.size();
+        int hits = 0;
+        for(int currentPageNumber = 0; currentPageNumber < pagesCount; currentPageNumber++) { // std::find заменить хэш таблицей
+
+            lookup_update(pages, currentPageNumber, hits);
+        }
+        return hits;
+    }
+
+    private:
+
+    void lookup_update (std::vector<TPage> &pages, int &currentPageNumber, int &hits) {
+
+        if (std::find(cache.begin(), cache.end(), pages[currentPageNumber]) == cache.end()) {
+
+            if (cache.size() < sz) {
+
+                cache.push_back(pages[currentPageNumber]);
+            }
+
+            else {
+
+                auto inpIt = pages.begin();
+                std::advance(inpIt, currentPageNumber + 1);
+
+                auto max = std::find(inpIt, pages.end(), cache[0]), del = cache.begin();
+                std::vector<TPage>::iterator pos;
+
+                if (max != pages.end()) {
+
+                    for (auto cacheIt = std::next(cache.begin()); cacheIt != cache.end(); ++cacheIt) {
+
+                        pos = std::find(inpIt, pages.end(), *cacheIt);
+
+                        if (max < pos) {
+                            max = pos;
+                            del = cacheIt;
+                        }
+
+                        if (max == pages.end()) {
+
+                            break;
+                        }
+                    }
+                }
+
+                if (std::find(inpIt, pages.end(), pages[currentPageNumber]) < max) {
+                    cache.erase(del);
+                    cache.push_back(pages[currentPageNumber]);
+                }
+            }
+            return;
+        }
+
+        // auto hitsCp = hits;
+
+        hits++;
+
+        // if (hits == hitsCp) printf("\nDOLBOEB\n");
+    }
+};
 
 #endif // IDEAL_CACHE_HPP
--- cache LRU 2Q/mainId.cpp
+++ cache LRU 2Q/mainId.cpp
@@ -1,14 +1,39 @@
 #include "ideal_cache.hpp"
+#include "LRU-2Q.hpp"
+#include <vector>
 #include <iostream>
-// #include "time.h"
-
-using namespace std;
 
 int main() {
+    int sz;
+    std::cin >> sz;
+    if (std::cin.bad()) {
+        std::cerr << "Incorrect input size cache\n";
+        return 1;
+    }
 
-    // clock_t start = clock();
-    cout << count_hits_Ideal_cache() << endl;
-    // clock_t end = clock();
+    std::vector<TPage> data;
+    idealCache cache(sz);
 
-    // cout << (end - start) / CLOCKS_PER_SEC << endl;
+    std::cout << "cache max_size - " << cache.cache.max_size() << std::endl;
+
+    int num;
+    std::cin >> num;
+    if (std::cin.bad()) {
+        std::cerr << "Incorrect input size \n";
+        return 1;
+    }
+
+    int val;
+    for(int i = 0; i < num; i++) {
+        std::cin >> val;
+        if (std::cin.bad()) {
+        std::cerr << "Incorrect input value\n";
+        return 1;
+    }
+        data.push_back(val);
+    }
+
+    int hits;
+    hits = cache.check_hits_ideal_cache(data);
+    std::cout << "Number of hits: " << hits << '\n';
 }
